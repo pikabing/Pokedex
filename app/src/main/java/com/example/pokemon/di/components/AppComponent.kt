8b0661e7702c1e739d4fe1230a03028a2d36d226
pikabing <pgcool33@gmail.com>
2020-02-06 12:56:45 +0530

    redundant methods removed from AppComponent

--- app/src/main/java/com/example/pokemon/di/components/AppComponent.kt
+++ app/src/main/java/com/example/pokemon/di/components/AppComponent.kt
@@ -2,13 +2,11 @@ package com.example.pokemon.di.components
 
 import android.app.Application
 import com.example.pokemon.MyApplication
-import com.example.pokemon.data.repository.PokemonRepository
 import com.example.pokemon.di.modules.*
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
-import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Singleton
@@ -16,9 +14,6 @@ import javax.inject.Singleton
     modules = [AndroidSupportInjectionModule::class, ActivityBindingModule::class, ApiModule::class, DbModule::class, AppModule::class]
 )
 interface AppComponent : AndroidInjector<MyApplication> {
-
-    fun providePokemonRepository(): PokemonRepository
-    fun provideRetrofit(): Retrofit
 
     @Component.Builder
     interface Builder {
